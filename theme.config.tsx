20bb38fc4d6263446de89910bd9d08977f1ca4fd
haveaniceknight <andrewxcoder@gmail.com>
2024-03-18 15:50:25 -0700

    New

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  logo: <span>Nice Comment</span>,
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
