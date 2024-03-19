3833e33413f5a90948fc344e3f0e7f12268063d6
haveaniceknight <andrewxcoder@gmail.com>
2024-03-19 00:36:23 -0700

    new

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,8 +5,13 @@ const config: DocsThemeConfig = {
   logo: <span>Nice Comment</span>,
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Andrew Hull',
   },
+  sidebar: {
+    autoCollapse: false,
+    // defaultMenuCollapseLevel can be set to Infinity if you want all folders expanded.
+    defaultMenuCollapseLevel: Infinity,
+  }
 }
 
 export default config
